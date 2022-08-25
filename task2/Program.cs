80aefaf0ec4beaf8141eea6697076f5eb4c93097
Alex <newalexis777@gmail.com>
2022-08-25 23:11:54 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите растояние от слона до друга: ");
+int x = int.Parse(Console.ReadLine());
+int y = 0;
+int z = 0;
+int step = 0;
+for (step = -1; y != x; step++)
+{
+    y = y + z;
+    z = 0;
+    while(z < 5)
+    {
+        
+        if(y + z == x)
+        {
+            break;
+        }
+        else
+        {
+            z++;
+        }
+ 
+    }
+}
+
+Console.WriteLine("Минимальное количество шагов которые нужно сделать == " + step + " step");
